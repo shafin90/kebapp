0c52d049cd9a3923bc71e018da0f95e00e840bed
maxpoolpixie <chakri.medcv@gmail.com>
2025-05-21 06:22:07 -0700

    added see full list button to our menu section

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { motion, useScroll, useSpring } from 'framer-motion'
+import { Link } from 'react-router-dom'
 import { 
   BeakerIcon, 
   HeartIcon, 
@@ -1085,6 +1086,26 @@ const Home = () => {
                 </motion.div>
               ))}
           </div>
+
+          {/* See Full Menu Link */}
+          <motion.div 
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ delay: 0.5 }}
+            className="mt-16 text-center"
+          >
+            <Link 
+              to="/menu" 
+              className="inline-flex items-center gap-2 bg-primary text-white px-8 py-4 rounded-xl 
+                         hover:bg-primary/90 transition-all group text-lg font-medium"
+            >
+              See Full Menu
+              <ArrowRightIcon className="w-5 h-5 group-hover:translate-x-1 transition-transform" />
+            </Link>
+            <p className="mt-4 text-text/70">
+              Discover our complete selection of dishes and beverages
+            </p>
+          </motion.div>
         </div>
       </section>
 
